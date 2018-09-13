b67c4206a69290af01e3a7b14a8181414cd3919c
wanglin <wanglin@zhongan.io>
2018-09-13 13:37:27 +0800

    补充API拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // 创建axios实例
-const service = aixos.create({
+const service = axios.create({
   // baseURL: 'http: www.baidu.com', // api 的 base_url,一般不设置，因为开发阶段一般都是proxy，生产环境都是nginx设，置了无法没法正常代理
   timeout: 5000
 })
@@ -21,10 +21,10 @@ service.interceptors.request.use(
 
 // repose 拦截器
 service.interceptors.response.use(
-  reponse => {
+  response => {
     // 这里可以根据返回码做一些事情，如401重登陆什么
     const res = response.data
-    
+
     return res
   },
   error => {
